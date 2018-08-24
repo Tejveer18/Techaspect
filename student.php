53b31c10734997b448a00f410c5b997c7c2c0ddc
Tejveer18 <41485991+Tejveer18@users.noreply.github.com>
2018-08-24 18:29:50 +0530

    Update student.php

--- student.php
+++ student.php
@@ -5,7 +5,7 @@ $branch=$_POST["BRANCH"];
 $year=$_POST["YEAR"];
 $sem=$_POST["SEM"];
 $prev_sem_per=$_POST["PREV_SEM_PER"];
-$conn=mysqli_connect("localhost","root","","test");
+$conn=mysqli_connect("localhost","root","");
 $sql="INSERT INTO `Student details`(`NAME`, `AGE`, `BRANCH`, `YEAR`, `SEM`, `PREV_SEM_PER`) VALUES ('$name','$age','$branch','$year','$sem','$prev_sem_per')";
 $result=mysqli_query($conn,$sql);
 echo "Successful";
