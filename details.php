461f7dde31828a3305150e4d50fa46cb0c991d7d
Tejveer18 <41485991+Tejveer18@users.noreply.github.com>
2018-08-24 18:24:56 +0530

    Create details.php

---
+++ details.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+}
+?>
+<html>
+<head>
+<title></title>
+        <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<form action="student.php" method="post">
+Name:<input type="text" name="NAME"><br>
+Age:<input type="text" name="AGE"><br>
+Branch:<input type="text" name="BRANCH"><br>
+Year:<input type="text" name="YEAR"><br>
+Sem:<input type="text" name="SEM"><br>
+Previous Semester Percentage:<input type="text" name="PREV_SEM_PER"><br>
+</body>
+</html>
+
